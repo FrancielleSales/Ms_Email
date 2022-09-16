6812cd333d1b339ac0cc167e4e8cf2312c733121
Francielle Sales <francielle_sales@hotmail.com>
2022-09-15 22:06:23 -0300

    Ms-Email

---
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -0,0 +1,56 @@
+package com.ms.email.services;
+
+import com.ms.email.enums.StatusEmail;
+import com.ms.email.models.EmailModel;
+import com.ms.email.repositories.EmailRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class EmailService {
+
+    @Autowired
+    EmailRepository emailRepository;
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public EmailModel sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailTo());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        }catch (MailException e){
+            log.error("sendEmail() - Email not sent");
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        }finally {
+            return emailRepository.save(emailModel);
+        }
+
+    }
+
+    public Page<EmailModel> findAll(Pageable pageable){
+        return emailRepository.findAll(pageable);
+    }
+
+    public Optional<EmailModel> findById(UUID emailId){
+        return emailRepository.findById(emailId);
+    }
+}
